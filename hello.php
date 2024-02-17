63427c34756a77f33ab4a2698de37775b0b8d810
fitzgallo <fitzgallo_gallo@rcc.edu.ph>
2024-02-17 15:53:07 +0800

    My first repo

---
+++ hello.php
@@ -0,0 +1,37 @@
+<html>
+    <header>
+        <title> Hello php </title>
+</header>
+<body>
+    <h1>
+        <?php
+$x= "hello";
+
+ if(is_int($x)){
+    echo "$x is Integer";
+ } elseif(is_int ($x)){
+    echo "$x is not integer";
+ } elseif(is_string ($x)){
+    echo "$x is a string";
+ }  elseif(is_float ($x)){
+    echo "$x is a float";
+
+ }
+
+ 
+   
+
+
+
+
+
+
+?> 
+
+<p> </p>
+
+ 
+
+    </h1>
+</body>
+    </html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "myself, super";
+echo "I hate $txt!";
+?>
+
+</body>
+</html>
